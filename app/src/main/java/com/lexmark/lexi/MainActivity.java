db17488cb94ecc27a75ebec39726b38fc8c7d038
Dave Mitchell <dmitchek@gmail.com>
2016-04-07 23:22:52 -0400

    Added chat functionality with text-to-speech

--- app/src/main/java/com/lexmark/lexi/MainActivity.java
+++ app/src/main/java/com/lexmark/lexi/MainActivity.java
@@ -1,67 +1,128 @@
 package com.lexmark.lexi;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
-    private EditText mUrl;
+    private EditText mRequestText;
+    private ViewGroup mChatLogs;
+    private TextToSpeech mTextToSpeech;
 
     private final String SCAN_RESULT = "SCAN_RESULT";
     private final String SCAN_MODE = "SCAN_MODE";
     private final String QR_CODE_MODE = "QR_CODE_MODE";
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private final int REQ_CODE_QR_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mWebView = (WebView)findViewById(R.id.webview);
-        mWebView.setWebViewClient(new WebViewClient());
-        mWebView.loadUrl("http://www.google.com");
+        mChatLogs = (ViewGroup)findViewById(R.id.chatLogs);
 
-        mUrl = (EditText)findViewById(R.id.url);
-
-        Button goBtn = (Button)findViewById(R.id.go_btn);
-        goBtn.setOnClickListener(new View.OnClickListener() {
+        ImageButton speekBtn = (ImageButton)findViewById(R.id.speek_btn);
+        speekBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                promptSpeechInput();
 
-                String url = mUrl.getText().toString();
+            }
+        });
 
-                if(url.length() > 0)
-                    mWebView.loadUrl(url);
+        mTextToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                mTextToSpeech.setLanguage(Locale.US);
             }
         });
 
     }
 
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Please say a command");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Text to speech not supported",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 0)
-        {
-            if(resultCode == RESULT_OK)
-            {
-                String url = data.getStringExtra(SCAN_RESULT);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
 
-                if(url.length() > 0)
-                    mUrl.setText(url);
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    addRequest(result.get(0));
+                    addResponse("This is my response to your command.");
+                }
+                break;
             }
-
+            case REQ_CODE_QR_CODE: {
+                // DO STUFF WITH QR CODE
+                break;
+            }
         }
+    }
+
+    private void addRequest(String text)
+    {
+        View newView = getLayoutInflater().inflate(R.layout.request_item, null);
+        TextView textView = (TextView)newView.findViewById(R.id.text);
+        textView.setText(text);
+        mChatLogs.addView(newView);
+    }
+
+    private void addResponse(String text)
+    {
+        View newView = getLayoutInflater().inflate(R.layout.response_item, null);
+        TextView textView = (TextView)newView.findViewById(R.id.text);
+        textView.setText(text);
+        mTextToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH, null, null);
+        mChatLogs.addView(newView);
     }
 
     @Override
